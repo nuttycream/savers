491245e041ca4d55d957950dc4b6053b8cef975a
nuttycream <issanutty@gmail.com>
2024-11-23 20:35:05 -0800

    feat: use main view instead of dock

--- savers.gd
+++ savers.gd
@@ -1,12 +1,25 @@
 @tool
 extends EditorPlugin
 
-var dock
+var main_view
+var singleton_name = "Savers"
 
 func _enter_tree() -> void:
-	dock = preload("res://addons/savers/saversdock.tscn").instantiate()
-	add_control_to_dock(DOCK_SLOT_LEFT_BL, dock)
+	add_autoload_singleton(singleton_name, "res://addons/savers/save_manager.gd")
 	
+	main_view = preload("res://addons/savers/saversdock.tscn").instantiate()
+	get_editor_interface().get_editor_main_screen().add_child(main_view)
+	_make_visible(false)
+
+func _has_main_screen() -> bool:
+	return true
+
+func _make_visible(vis: bool) -> void:
+		main_view.visible = vis
+
+func _get_plugin_name() -> String:
+	return "Savers"
+
 func _exit_tree() -> void:
-	remove_control_from_docks(dock)
-	dock.free()
+	remove_autoload_singleton(singleton_name)
+	main_view.queue_free()
