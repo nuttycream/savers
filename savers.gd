98ea7be6d7db46a90a1f7d561720c58a97860808
nuttycream <issanutty@gmail.com>
2024-11-25 17:50:23 -0800

    revert: back to dockable inst of main-view

--- savers.gd
+++ savers.gd
@@ -1,25 +1,16 @@
 @tool
 extends EditorPlugin
 
-var main_view
+var dock
 var singleton_name = "Savers"
 
 func _enter_tree() -> void:
 	add_autoload_singleton(singleton_name, "res://addons/savers/save_manager.gd")
 	
-	main_view = preload("res://addons/savers/saversdock.tscn").instantiate()
-	get_editor_interface().get_editor_main_screen().add_child(main_view)
-	_make_visible(false)
-
-func _has_main_screen() -> bool:
-	return true
-
-func _make_visible(vis: bool) -> void:
-		main_view.visible = vis
-
-func _get_plugin_name() -> String:
-	return "Savers"
+	dock = preload("res://addons/savers/saversdock.tscn").instantiate()
+	add_control_to_dock(DOCK_SLOT_RIGHT_BL, dock)
 
 func _exit_tree() -> void:
 	remove_autoload_singleton(singleton_name)
-	main_view.queue_free()
+	remove_control_from_docks(dock)
+	dock.queue_free()
