d4cc031b888bee5a3d03c0beb071515b0ddcad77
nuttycream <issanutty@gmail.com>
2024-12-10 22:43:39 -0800

    feat: impl ignore list

--- save_manager.gd
+++ save_manager.gd
@@ -3,12 +3,37 @@ extends Node
 
 # ideally we want this to be set by devs
 const SAVE_PATH := "user://save.json"
+const CONFIG_PATH := "res://addons/savers/config.json"
+
 var save_nodes: Array[Node] = []
+var ignored_vars: Dictionary = {}
 
 func _ready() -> void:
 	await get_tree().process_frame
+	load_config()
 	save_nodes = get_vars(get_tree().root)
 	load_game()
+
+func load_config() -> void:
+	if not FileAccess.file_exists(CONFIG_PATH):
+		ignored_vars = {}
+		return
+		
+	var json := JSON.new()
+	var err := json.parse(FileAccess.open(CONFIG_PATH, FileAccess.READ).get_as_text())
+	
+	if err != OK:
+		print("failed to load config")
+		ignored_vars = {}
+		return
+		
+	ignored_vars = json.get_data()
+
+func save_config() -> void:
+	FileAccess.open(CONFIG_PATH, FileAccess.WRITE).store_string(
+		JSON.stringify(ignored_vars, "  ")
+	)
+	print("config saved")
 
 func get_vars(root: Node) -> Array[Node]:
 	var nodes: Array[Node] = []
@@ -24,6 +49,24 @@ func get_vars(root: Node) -> Array[Node]:
 	
 	return nodes
 
+func is_var_ignored(node: Node, var_name: String) -> bool:
+	var script_path = node.get_script().resource_path
+	return script_path in ignored_vars and var_name in ignored_vars[script_path]
+
+func add_ignored_var(script_path: String, var_name: String) -> void:
+	if not script_path in ignored_vars:
+		ignored_vars[script_path] = []
+	if not var_name in ignored_vars[script_path]:
+		ignored_vars[script_path].append(var_name)
+		save_config()
+
+func remove_ignored_var(script_path: String, var_name: String) -> void:
+	if script_path in ignored_vars and var_name in ignored_vars[script_path]:
+		ignored_vars[script_path].erase(var_name)
+		if ignored_vars[script_path].is_empty():
+			ignored_vars.erase(script_path)
+		save_config()
+
 func save() -> void:
 	var save_data := {}
 	
@@ -31,14 +74,17 @@ func save() -> void:
 		if not is_instance_valid(node): continue
 		
 		var props := {}
+		var node_path := str(node.get_path())
 		for prop in node.get_script().get_script_property_list():
-			props[prop["name"]] = node.get(prop["name"])
+			if not is_var_ignored(node, prop["name"]):
+				props[prop["name"]] = node.get(prop["name"])
 		
-		save_data[node.get_path()] = {
-			"scene_path": node.scene_file_path,
-			"node_path": str(node.get_path()),
-			"properties": props
-		}
+		if not props.is_empty():
+			save_data[node_path] = {
+				"scene_path": node.scene_file_path,
+				"node_path": node_path,
+				"properties": props
+			}
 	
 	FileAccess.open(SAVE_PATH, FileAccess.WRITE).store_string(
 		JSON.stringify(save_data, "  ")
@@ -68,6 +114,7 @@ func load_game() -> void:
 			continue
 			
 		for prop_name in data.get("properties", {}):
-			node.set(prop_name, data["properties"][prop_name])
+			if not is_var_ignored(node, prop_name):
+				node.set(prop_name, data["properties"][prop_name])
 			
 	print("loaded save")
--- saversdock.gd
+++ saversdock.gd
@@ -2,14 +2,32 @@
 extends Control
 
 var script_list: ItemList
+var ignore_list: ItemList
 var fs: EditorFileSystem
+var savers: Node
 
 func _ready() -> void:
 	script_list = $VBoxContainer/ScriptList
+	ignore_list = $VBoxContainer/IgnoreList
+	
 	var refresh_btn = $VBoxContainer/RefreshButton
 	refresh_btn.pressed.connect(do_scan)
 	
+	var add_ignore_btn = $VBoxContainer/HBoxContainer/AddIgnoreButton
+	add_ignore_btn.pressed.connect(add_to_ignore)
+	
+	var remove_ignore_btn = $VBoxContainer/HBoxContainer/RemoveIgnoreButton
+	remove_ignore_btn.pressed.connect(remove_from_ignore)
+	
 	fs = EditorInterface.get_resource_filesystem()
+
+	await get_tree().process_frame
+	savers = get_node("/root/Savers")
+	refresh_lists()
+
+func refresh_lists() -> void:
+	do_scan()
+	update_ignore_list()
 
 func do_scan() -> void:
 	script_list.clear()
@@ -44,3 +62,31 @@ func scan_script(path: String) -> void:
 		for prop in script.get_script_property_list():
 			if prop["usage"] & PROPERTY_USAGE_EDITOR:
 				script_list.add_item("%s: %s (%s)" % [path, prop["name"], prop["type"]])
+				script_list.set_item_metadata(script_list.get_item_count() - 1, {
+					"path": path,
+					"var": prop["name"]
+				})
+
+func update_ignore_list() -> void:
+	ignore_list.clear()
+	for node_path in savers.ignored_vars:
+		for var_name in savers.ignored_vars[node_path]:
+			ignore_list.add_item("%s: %s" % [node_path, var_name])
+			ignore_list.set_item_metadata(ignore_list.get_item_count() - 1, {
+				"path": node_path,
+				"var": var_name
+			})
+
+func add_to_ignore() -> void:
+	var selected = script_list.get_selected_items()
+	for idx in selected:
+		var metadata = script_list.get_item_metadata(idx)
+		savers.add_ignored_var(metadata["path"], metadata["var"])
+	update_ignore_list()
+
+func remove_from_ignore() -> void:
+	var selected = ignore_list.get_selected_items()
+	for idx in selected:
+		var metadata = ignore_list.get_item_metadata(idx)
+		savers.remove_ignored_var(metadata["path"], metadata["var"])
+	update_ignore_list()
